4bde6c242109ecb4cd61b948745cf44721ed1f47
Everett Cooley <39889137+EverettCooley@users.noreply.github.com>
2022-09-22 15:48:34 -0700

    Update bm.py

--- bm.py
+++ bm.py
@@ -1,6 +1,5 @@
 import csv
 import math
-
 class BM_25(object):
     def __init__(self, dataFile):
         self.K1 = 1.2
@@ -14,7 +13,7 @@ class BM_25(object):
         self.AVERAGE_DOC_LENGTH = self.average_length_of_rows()
         self.INVERTED_INDEX = self.create_inverted_index()
 
-    
+
     def create_inverted_index(self):
         inverted_index = {}
         for row in self.ROWS:
@@ -37,8 +36,6 @@ class BM_25(object):
         sum = 0
         for i in range(self.N_OF_DOCS):
             sum += self.length_of_row(i)
-            # print("sum", sum)
-            # print(i)
         return sum/self.N_OF_DOCS
 
 
@@ -61,13 +58,10 @@ class BM_25(object):
     def bm25_single_doc(self, query_list, docID):
         sum = 0
         for term in query_list:
-            print("term: ",term)
             inverted_doc = self.inverted_doc_freq(term)
             term_f = self.term_freq(docID, term)
             qtf =  self.query_term_frequency(query_list, term)
             sum += inverted_doc * term_f * qtf
-            print("sum",sum)
-            print()
         return sum
 
 
@@ -77,45 +71,29 @@ class BM_25(object):
             df_t = len(self.INVERTED_INDEX[t])
         else:
             df_t=0
-        print("n = ", n)
-        print("df_t = ", df_t)
-        return math.log(n-df_t+.5) - math.log(df_t+.5)
+        return math.log((n-df_t+.5)/(df_t+.5))
     
 
     def query_term_frequency(self, query_list, t):
-        count = query_list.count(t)
-        print("qf_t = ", count)
-        return (((self.K2+1)*count)/(self.K2+count))
+        qft = query_list.count(t)
+        return (((self.K2+1)*qft)/(self.K2+qft))
 
 
     def term_freq(self, docID, t):
         row = list(filter(None, self.ROWS[docID][1].split(" ")))
-        count = row.count(t)
-        print("f_t = ",count)
-        print("average_doc_length = ",self.AVERAGE_DOC_LENGTH)
-        print("len_of_doc = ", len(row))
-        top = (self.K1 + 1)*count
+        ft = row.count(t)
+        top = (self.K1 + 1)*ft
         bot_inside = (1-self.B)+(self.B*(len(row)/self.AVERAGE_DOC_LENGTH))
-        bot = (self.K1*bot_inside)+count
+        bot = (self.K1*bot_inside)+ft
         return top / bot
 
 
-if __name__ == "__main__":
+def main():
     bm = BM_25("wine.csv")
-    # print(bm.INVERTED_INDEX)
-    # print(bm.AVERAGE_DOC_LENGTH)
-    # print(bm.MAX_DOC_ID)
-    # print(bm.N_OF_DOCS)
-    # print(bm.ROWS)
-    # print(bm.bm25("tremendous tremendous watson", 5))
-    print(bm.bm25_single_doc(["tremendous", "tremendous", "watson"], 71))
-    # print(bm.average_length_of_rows())
-    # print(bm.inverted_doc_freq("there"))
+    print(bm.bm25("tremendous tremendous watson", 5))
+    # print(bm.bm25_single_doc(["tremendous", "tremendous", "watson"], 71))
 
 
-    # print(bm.query_term_frequency("tremendous tremendous watson", "tremendous"))
-    # print(bm.term_freq(2, "watson"))
-
-# Use k1 = 1.2,
-# k2 = 500, and b = 0.75
-# 
+if __name__ == "__main__":
+    main()
+    
